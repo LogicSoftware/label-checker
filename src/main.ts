ad90b3ee9c87b0c581556a583dabf2056b6043fb
Denis Borovnev <denis.borovnev@gmail.com>
2022-01-21 13:56:04 +0300

    init
    try to block merge until tested label is not added to pr

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
+  if (!github.context.payload.pull_request) {
+    return
+  }
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const config = getConfig()
+    const client = github.getOctokit(config.githubToken)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pullRequest = await client.rest.pulls.get({
+      ...github.context.repo,
+      pull_number: github.context.payload.pull_request.number
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    const actualLabels = pullRequest.data.labels.map(x => x.name)
+    const isOk = config.anyOfLabels.some(label => actualLabels.includes(label))
+    core.exportVariable('labels_check_passed', isOk)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+interface Config {
+  githubToken: string
+  anyOfLabels: string[]
+}
+
+const getConfig = (): Config => {
+  const githubToken = core.getInput('github-token')
+  const anyOfLabelsText = core.getInput('any_of')
+  const anyOfLabels = anyOfLabelsText.split(',').map(l => l.trim())
+
+  return {
+    githubToken,
+    anyOfLabels
+  } as const
 }
 
 run()
