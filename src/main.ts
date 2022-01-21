39ba2c9179ebb432f3d588539bc64bce319ed877
Denis Borovnev <denis.borovnev@gmail.com>
2022-01-21 13:57:35 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+// test
 async function run(): Promise<void> {
   if (!github.context.payload.pull_request) {
     return
